f83b5127aee4ebc7e695d2dc79362be1e15b44dd
ytyaru <ytyaru@outlook.jp>
2019-08-14 12:41:58 +0900

    LIMIT連番追加

--- src/run.sh
+++ src/run.sh
@@ -2,6 +2,7 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd "$SCRIPT_DIR"
 sqlite3 :memory: \
 ".load ./build/series" \
+"SELECT * FROM generate_series LIMIT 5;" \
 "SELECT value FROM generate_series(5,100,5);" \
 "SELECT random() FROM generate_series LIMIT 20;" \
 "SELECT (abs(random()) % 100) FROM generate_series LIMIT 20;"
